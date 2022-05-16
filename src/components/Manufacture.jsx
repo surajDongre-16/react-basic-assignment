542f6d112558adbc393337bff6c4663712f6f2f2
surajDongre-16 <surajdongre8@gmail.com>
2022-05-16 23:00:53 +0530

    basic assignment added

---
+++ src/components/Manufacture.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Manufacture = () => {
+
+    return (
+        <div>
+            <h1>Mobile Manufacturers</h1>
+            <ul>
+                <li>Samsung</li>
+                <li>HTC</li>
+                <li>Micromax</li>
+                <li>Apple</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Manufacture
---
+++ src/components/MobileOS.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Mobile = () =>{
+    return (
+        <div>
+            <h1>Mobile Operating System</h1>
+            <ul>
+                <li>Android</li>
+                <li>BlckBerry</li>
+                <li>iPhone</li>
+                <li>Windows Phone</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Mobile
